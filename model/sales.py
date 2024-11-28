823f13aed2afa6aa32cfdca7f16bb368ced12bea
Anthony <ndegwaanthony300@gmail.com>
2024-11-28 17:00:38 +0300

    initialization of te prodiction model folder

---
+++ model/sales.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+from sklearn.linear_model import LinearRegression, LogisticRegression
+from sklearn.svm import SVC, SVR
+from sklearn.tree import DecisionTreeClassifier, DecisionTreeRegressor
+from sklearn.metrics import confusion_matrix
+import matplotlib.pyplot as plt
+import seaborn as sns
